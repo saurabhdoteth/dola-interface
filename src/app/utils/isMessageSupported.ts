ef6056bed40729de26fb0d3ace85bd70ee8c751f
saurabh <saurabh@untitledlabs.io>
2023-12-09 18:30:03 +0530

    ft: util to check supported message

---
+++ src/app/utils/isMessageSupported.ts
@@ -0,0 +1,10 @@
+import type { CachedMessageWithId } from "@xmtp/react-sdk";
+import { ContentTypeId, ContentTypeText } from "@xmtp/react-sdk";
+
+/**
+ * Determines if a message is supported by the app
+ */
+export const isMessageSupported = (message: CachedMessageWithId) => {
+  const contentType = ContentTypeId.fromString(message.contentType);
+  return contentType.sameAs(ContentTypeText);
+};
